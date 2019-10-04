7016683e4c15416a32e7304776932bd32d45a70f
garciazapiain <juangarciazapiain@gmail.com>
2019-10-04 10:41:44 +0200

    firststage

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,28 @@
+let mouth = true;
+let xpos = 0;
+const TILE_SIZE = 85;
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('it works');
+  const update = () => {
+    const pacboy = document.querySelector('.entity--pac');
+    pacboy.style.left = `${xpos}px`;
+  }
+  document.addEventListener('keydown',()=>{
+    const pacboylight=document.querySelector('.pacboy-active-light');
+      if (event.key === 'ArrowRight'){
+        if (mouth===true){
+          pacboylight.style.backgroundPositionX = 'right';
+          mouth=false;
+        }
+        else {
+          pacboylight.style.backgroundPositionX = 'left';
+          mouth=true;
+        }
+      xpos += TILE_SIZE;
+      update();
+      }
+  })
+
 });
+
+
